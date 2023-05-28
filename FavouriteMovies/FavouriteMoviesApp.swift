40329ba4133197acc2191df9b98b4bf9aef76495
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-05-28 17:33:47 -0400

    Initial Commit

---
+++ FavouriteMovies/FavouriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteMoviesApp.swift
+//  FavouriteMovies
+//
+//  Created by Kiho Okazawa on 2023-05-28.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
