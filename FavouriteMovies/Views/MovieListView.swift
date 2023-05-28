437b76458f2f73dd04a1fe20b1e7878209f221af
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-05-28 17:35:58 -0400

    Deleted a content view and seated up a new view called ModelListView

---
+++ FavouriteMovies/Views/MovieListView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListView.swift
+//  FavouriteMovies
+//
+//  Created by Kiho Okazawa on 2023-05-28.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
