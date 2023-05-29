4e48b9d31c7ffb6313262f30289f191d3643e955
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-05-28 22:22:58 -0400

    !!Fixed version. Made the list of movies read from the database instead of using a static layout.

--- FavouriteMovies/Views/MovieListView.swift
+++ FavouriteMovies/Views/MovieListView.swift
@@ -23,7 +23,7 @@ struct MovieListView: View {
             List(movies.results) { currentMovie in
                 MovieItemView(name: currentMovie.name, genre: currentMovie.genre,
                               rating: currentMovie.rating)
-               
+                
             }
             .navigationTitle("Favorite Movies")
         }
@@ -34,7 +34,7 @@ struct MovieListView_Previews: PreviewProvider {
     static var previews: some View {
         MovieListView()
         //Make the database available to all other view through the envirnment
-        .environment(\.blackbirdDatabase, AppDatabase.instance)
-            
+            .environment(\.blackbirdDatabase, AppDatabase.instance)
+        
     }
 }
